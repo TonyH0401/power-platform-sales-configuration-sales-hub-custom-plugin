63805c424893291e5e047cff987bd982f8adbab0
Phu Le Gia <Phu.LeGia@selfstudy.store>
2025-04-17 11:22:38 +0100

    some edits

---
+++ ShOpportunity/ShOpportunityDuplicate.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Xrm.Sdk;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShOpportunity
+{
+    public class ShOpportunityDuplicate : IPlugin
+    {
+        public void Execute(IServiceProvider serviceProvider)
+        {
+            IPluginExecutionContext context = (IPluginExecutionContext)serviceProvider.GetService(typeof(IPluginExecutionContext));
+            // IOrganizationServiceFactory serviceFactory = 
+        }
+    }
+}
